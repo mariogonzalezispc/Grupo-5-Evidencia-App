0b827ea8b434dafdc965cdfc28359ffd72d48185
Felipe Rebolledo <feliperebolledo@outlook.com>
2023-06-19 19:31:19 -0300

    eliminar fragments y codigo inutil

--- EcoDoctapp/app/src/main/java/com/cdp/ecodoctapp/MainActivity.java
+++ EcoDoctapp/app/src/main/java/com/cdp/ecodoctapp/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         navigationView = binding.navView;
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.activity_reproductor_video)
+                R.id.nav_home, R.id.activity_reproductor_video)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
